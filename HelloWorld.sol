ad5f5ecba3593cd5dd1de401a57d4584e2460fc4
blackjack9995 <alltrash123456@gmail.com>
2022-05-25 09:33:45 +0530

    'core'

---
+++ HelloWorld.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.4.16 <0.9.0;
+
+contract HelloWorld {
+    function Hello() pure public returns(string memory) {
+        return ('Hello World');
+    }
+}
+
+
